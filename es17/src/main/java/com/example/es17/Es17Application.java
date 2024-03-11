797b3ea00cf4c21e373792eae7214097945320ac
udhin98 <udhin1998@gmail.com>
2024-03-11 13:26:18 +0100

    es17

---
+++ es17/src/main/java/com/example/es17/Es17Application.java
@@ -0,0 +1,13 @@
+package com.example.es17;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Es17Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Es17Application.class, args);
+	}
+
+}
---
+++ es17/src/test/java/com/example/es17/Es17ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.es17;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Es17ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
